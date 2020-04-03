7d9caf48ecf90ccea80c1ddb7284de520fa56644
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2020-04-03 17:23:12 +0200

    work on parsing data

--- hook/ftrack_usd_action.py
+++ hook/ftrack_usd_action.py
@@ -6,9 +6,13 @@ import logging
 
 from ftrack_action_handler.action import BaseAction
 import ftrack_api
+import sys
+sys.path.append('/home/ftrackwork/.local/lib/python')
+
+from pxr import Usd, UsdGeom
 
 
-class EditDescriptions(BaseAction):
+class USDAction(BaseAction):
     '''Action to allow updating the descriptions on AssetVersion Objects.'''
     label = 'Usd builder'
     identifier = 'com.ftrack.recipes.usd_builder'
@@ -16,30 +20,45 @@ class EditDescriptions(BaseAction):
 
     def discover(self, session, entities, event):
         # available on any entity
+        if not entities:
+            return False
+        
         return True
 
     def launch(self, session, entities, event):
-        for id_, comment in event['data']['values'].items():
-            session.get('AssetVersion', id_)['comment'] = comment
-        session.commit()
+        
+        entity_type, entity_id = entities[0]
+        root = self.session.get(entity_type, entity_id)
+
+        while root['descendants']:
+            for child in root['descendants']:
+                placement =  '/'.join([link['name'] for link in child['link']])
+                print placement
+                asset_versions = session.query(
+                    'select components from AssetVersion where task.id is "{}"'.format(child['id'])
+                ).all()
+                for asset_version in asset_versions:
+                    for component in asset_version['components']:
+                        print component
+                        # file_path = None
+                        # try:
+                        #     file_path =  location.get_filesystem_path(component)
+                        # except Exception as error:
+                        #     print error
+                        #     pass
+
+                        # if file_path and os.path.splitext(filepath)[-1] == '.usd':
+                        #     print filepath
+
+                root = child
 
         return {
             'success': True,
-            'message': 'Description(s) updated.'
+            'message': 'Done.'
         }
 
 def register(session, **kw):
     if not isinstance(session, ftrack_api.Session):
         return
-
-    action = EditDescriptions(session)
+    action = USDAction(session)
     action.register()
-
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    session = ftrack_api.Session(auto_connect_event_hub=True)
-    register(session)
-    logging.info(
-        'Registered actions and listening for event. Use Ctrl-C to abort.'
-    )
-    session.event_hub.wait()
